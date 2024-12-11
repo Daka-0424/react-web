2532aead7844374b83e182bced075c09a2ead931
Daka-0424 <userjh41001@gmail.com>
2024-12-11 11:17:47 +0900

    fix: popup

--- src/my/Rout.js
+++ src/my/Rout.js
@@ -1,10 +1,16 @@
+import React, { useState } from "react";
 import '../assets/css/Info.css';
 import TitleImageWebp from '../assets/img/img1.webp';
 import TitleImageJpg from '../assets/img/img1.jpg';
 import Header from './partial/header';
+//import Popup from '../Popup';
 
 export default function App() {
-  const Motto = "aaa";
+  const [isPopupVisible, setPopupVisible] = useState(false);
+  const togglePopup = () => {
+    setPopupVisible(!isPopupVisible);
+  };
+
   return (
     <div id="root" class="wrapper">
       <Header />
@@ -13,6 +19,15 @@ export default function App() {
         <source srcset={TitleImageJpg} type="image/jpeg" />
         <img class="title-img"/>
       </picture>
+      {/*<button onClick={togglePopup}>Popupを開くでごわす！</button>
+      {isPopupVisible && (
+        <Popup>
+          <div class="popup">
+            <p>ここにポップアップの内容を記載するでごわす！</p>
+            <button onClick={togglePopup}>Popupを閉じるでごわす！</button>
+          </div>
+        </Popup>
+      )}*/}
     </div>
   );
 }
