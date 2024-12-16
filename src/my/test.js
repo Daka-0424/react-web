902da7f670dcb0f9f82f78428922d6fe2b203610
Daka-0424 <userjh41001@gmail.com>
2024-12-16 18:25:45 +0900

    fix: popup test

--- src/my/test.js
+++ src/my/test.js
@@ -1,64 +1,34 @@
-/*import './styles.css';
-import * as THREE from 'three';
-// Render
-import { useMemo } from 'react';
-import { OrbitControls } from '@react-three/drei/core/OrbitControls';
-// FPS
-import { Stats } from '@react-three/drei/core/Stats';
-import { Canvas, useFrame } from '@react-three/fiber';
-import { vertexShader, fragmentShader } from './shader';
-
-const Particles = () => {
-  const planePositions = useMemo(() => {
-    const planeGeometry = new THREE.PlaneGeometry(6, 6, 128, 128);
-    const positions = planeGeometry.attributes.position.array;
-
-    return positions;
-  }, []);
-
-  const shaderArgs = useMemo(
-    () => ({
-      uniforms: {
-        uTime: { value: 0 }
-      },
-      vertexShader,
-      fragmentShader
-    }),
-    []
-  );
-
-  useFrame(() => {
-    shaderArgs.uniforms.uTime.value++;
-  }, []);
-
-  return (
-    <points rotation={[-Math.PI / 2, 0, 0]}>
-      <bufferGeometry attach="geometry">
-        <bufferAttribute
-          attach="attributes-position"
-          array={planePositions}
-          itemSize={3}
-          count={planePositions.length / 3}
-        />
-      </bufferGeometry>
-      <shaderMaterial
-        args={[shaderArgs]}
-        transparent
-        depthTest={false}
-        depthWrite={false}
-      />
-    </points>
-  );
-};
+import React, { useState } from "react";
+import '../assets/css/Info.css';
+import TitleImageWebp from '../assets/img/img1.webp';
+import TitleImageJpg from '../assets/img/img1.jpg';
+import Header from './partial/header';
+import Picture from '../partial/Picture';
+import Popup from '../partial/Popup';
 
 export default function App() {
+  const [isPopupVisible, setPopupVisible] = useState(false);
+  const togglePopup = () => {
+    setPopupVisible(!isPopupVisible);
+  };
+
   return (
-    <Canvas dpr={2}>
-      <color attach="background" args={[0xf5f3fd]} />
-      <OrbitControls makeDefault />
-      <Stats />
-      <Particles />
-    </Canvas>
+    <div id="root" class="wrapper">
+      <Header />
+      <Picture jpeg={TitleImageJpg} webp={TitleImageWebp}>
+        <img class="title-img" />
+      </Picture>
+      <button onClick={togglePopup}>Popupを開くでごわす！</button>
+      {isPopupVisible && (
+        <Popup>
+          <div class="popup">
+            <Picture jpeg={TitleImageJpg} webp={TitleImageWebp}>
+              <img style={{ width: '100%' }} />
+            </Picture>
+            <button onClick={togglePopup}>Popupを閉じるでごわす！</button>
+          </div>
+        </Popup>
+      )}
+    </div>
   );
 }
-*/
